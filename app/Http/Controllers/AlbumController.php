5acc52ac25aec74c8e3e254f9d0fbef76469bc91
ahmed256.fintech <ahmed256.fintech@gmail.com>
2022-10-15 20:19:15 +0200

    add rest functionality

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -5,18 +5,54 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Album;
 use App\Http\Requests\AlbumRequest;
+use Illuminate\Support\Facades\Storage;
 
 class AlbumController extends Controller
 {
     public function store(AlbumRequest $request)
     {
         $image_names = array_values($request->only('image_names'))[0];
-        
+
         $album = Album::create($request->except('image_names'));
-        foreach ($image_names as $image) {
-            $path = $image->storePublicly('albums', "public");
-            $image =  "/storage/" . $path;
-            $album->images()->create(['name' => $image]);
+
+        $this->storeImg($image_names, $album);
+    }
+
+    public function update(AlbumRequest $request, $id)
+    {
+        $image_names = array_values($request->only('image_names'))[0];
+
+        $album = Album::findOrFail($id);
+
+        $this->deleteImg($album);
+        $album->update($request->except('image_names'));
+        $this->storeImg($image_names, $album);
+    }
+
+    private function deleteImg($model)
+    {
+        foreach ($model->images as $image) {
+            // $path = 'storage/images/albums/'.$image?->name;
+            $path = preg_replace("/[\s\S]*storage/", '', $image?->name);
+            Storage::disk('public')->delete($path);
+            $image->delete();
         }
+    }
+
+
+    private function storeImg($image_names, $model)
+    {
+        foreach ($image_names as $image) {
+            $path = $image->storePublicly('images/albums', "public");
+            $image =  "/storage/" . $path;
+            $model->images()->create(['name' => $image]);
+        }
+    }
+
+    public function destroy($id)
+    {
+        $album = Album::findOrFail($id);
+        $this->deleteImg($album);
+        $album->delete();
     }
 }
